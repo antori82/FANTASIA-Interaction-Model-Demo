616a8334bbdd5d7bf6987ccdcbcb8ae96b46cc59
antori82 <antonio.origlia@unina.it>
2025-05-02 12:54:19 +0200

    Updated FANTASIATemplateTarget.cs to follow the latest Unreal Engine include order, as FANTASIA does

--- Source/FANTASIATemplate.Target.cs
+++ Source/FANTASIATemplate.Target.cs
@@ -8,8 +8,8 @@ public class FANTASIATemplateTarget : TargetRules
 	public FANTASIATemplateTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V4;
-		IncludeOrderVersion = EngineIncludeOrderVersion.Unreal5_3;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
+        IncludeOrderVersion = EngineIncludeOrderVersion.Latest;
 		ExtraModuleNames.Add("FANTASIATemplate");
 	}
 }
